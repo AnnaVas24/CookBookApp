6b6ee86a70f65e2b1b3f120b1b25ce6cc061c800
romanzhukovv <zhukov.roman89@gmail.com>
2022-01-03 01:55:27 +0300

    added models

---
+++ CookBookApp/Models/Contributor.swift
@@ -0,0 +1,34 @@
+//
+//  Contributor.swift
+//  CookBookApp
+//
+//  Created by Roman Zhukov on 03.01.2022.
+//
+
+struct Contributor {
+    let name: String
+    let surname: String
+    let emoji: String
+    let position: String
+    
+    var fullname: String {
+        "\(name) \(surname)"
+    }
+    
+    static func getContributor() -> [Contributor] {
+        [
+            Contributor(name: "Anna",
+                        surname: "Vasichko",
+                        emoji: "anya_pic",
+                        position: "Junior"),
+            Contributor(name: "Pavel",
+                        surname: "Kachan",
+                        emoji: "pavel_pic",
+                        position: "Team Lead"),
+            Contributor(name: "Roman",
+                        surname: "Zhukov",
+                        emoji: "roman_pic",
+                        position: "Junior")
+        ]
+    }
+}
